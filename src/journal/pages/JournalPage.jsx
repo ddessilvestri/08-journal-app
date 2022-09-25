cc2f198e9c65cedb7d4fd7160d72abf75dad55dd
ddessilvestri <silvestri.des@gmail.com>
2022-09-25 14:21:44 -0300

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,10 @@
-import {Typography} from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../view';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Consectetur sunt amet reprehenderit Lorem sint cillum. Id reprehenderit laboris in duis Lorem laboris. Nulla dolore minim et amet ex id eu dolore Lorem nisi Lorem cillum consectetur. Ex incididunt minim dolor consequat. Mollit velit labore incididunt esse nisi nisi fugiat non reprehenderit dolore. Non eiusmod occaecat incididunt laborum. Ex et aliquip dolore amet nisi fugiat ipsum exercitation.</Typography>
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
